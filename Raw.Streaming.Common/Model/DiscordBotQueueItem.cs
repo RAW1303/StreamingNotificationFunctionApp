a46b817d37517525198b0e6b4a71f41b621f8c91
Roy Weller <mail@royweller.co.uk>
2022-04-10 12:31:13 +0100

    Add serialization tags to DiscordBotQueueItem

--- Raw.Streaming.Common/Model/DiscordBotQueueItem.cs
+++ Raw.Streaming.Common/Model/DiscordBotQueueItem.cs
@@ -1,10 +1,13 @@
 ﻿using Raw.Streaming.Common.Model.Enums;
+using System.Text.Json.Serialization;
 
 namespace Raw.Streaming.Common.Model
 {
     public class DiscordBotQueueItem
     {
+        [JsonInclude]
         public MessageType Type { get; set; }
+        [JsonInclude]
         public Entity[] Entities { get; set; }
 
         public DiscordBotQueueItem(MessageType type, params Entity[] entities)
