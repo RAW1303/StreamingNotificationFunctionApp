35d6b665636d8347f56703afc18e8eec5938a445
Roy Weller <mail@royweller.co.uk>
2022-12-13 22:58:21 +0000

    Remove moved method

--- Raw.Streaming.Discord/Services/DiscordEventApiService.cs
+++ Raw.Streaming.Discord/Services/DiscordEventApiService.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
-using Raw.Streaming.Common.Model;
 using Raw.Streaming.Discord.Model.DiscordApi;
-using Raw.Streaming.Discord.Translators;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,23 +10,6 @@ namespace Raw.Streaming.Discord.Services;
 internal class DiscordEventApiService : BaseDiscordApiService, IDiscordEventService
 {
     public DiscordEventApiService(HttpClient httpClient, ILogger<DiscordEventApiService> logger) : base(httpClient, logger) { }
-
-    public async Task SyncScheduledEventsAsync(string guildId, IEnumerable<Event> events)
-    {
-        var existingEvents = await GetScheduledEventsAsync(guildId);
-        var botExistingEvents = existingEvents.Where(e => e.CreatorId == AppSettings.DiscordBotApplicationId);
-
-        var eventsToAdd = EventToDiscordGuildScheduledEventTranslator.Translate(events.Where(x => !botExistingEvents.Any(y => x.Url == y.EntityMetadata?.Location)));
-        var eventsToUpdate = EventToDiscordGuildScheduledEventTranslator.Merge(botExistingEvents, events);
-        var eventsToDelete = botExistingEvents.Where(x => !events.Any(y => x.EntityMetadata?.Location == y.Url));
-
-        var tasks = new List<Task>();
-        tasks.AddRange(eventsToAdd.Select(x => CreateScheduledEventAsync(guildId, x)));
-        tasks.AddRange(eventsToUpdate.Select(x => UpdateScheduledEventAsync(guildId, x.Id, x)));
-        tasks.AddRange(eventsToDelete.Select(x => DeleteScheduledEventAsync(x.GuildId, x.Id)));
-
-        await Task.WhenAll(tasks);
-    }
 
     public async Task<IEnumerable<GuildScheduledEvent>> GetScheduledEventsAsync(string guildId)
     {
